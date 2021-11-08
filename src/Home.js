1ace7b425d663f3e0e3fe988cbaa21583f71389c
Imani Taylor <imanir.taylor@gmail.com>
2021-11-08 14:51:09 -0600

    added links to buttons

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Route, Switch } from "react-router-dom";
-import Layout from "./Layout";
+import { Route, Switch, Link } from "react-router-dom";
+
 import { listDecks } from "../src/utils/api";
+
+
 /*
 Include the Layout screen
 The path to this screen should be /.
@@ -26,42 +28,46 @@ function Home() {
     return () => ac.abort();
   }, [setDecks]);
 
-  if (decks.length > 0) {
-    decks.map((deck) => console.log(deck.name));
-  } else console.log("no deck");
+ 
+//   const handleClick = (event) => {
+//     event.preventDefault();
+//     console.log("Submitted");
+//   };
 
   return (
     <div className="app-routes">
+
       <Switch>
-        <Route exact path="/">
-          <Layout />
+        <Route path="/">
+          
           <div className="container">
-            <button type="button" className="btn btn-secondary btn-lg">
-              + Create Deck
-            </button>
-          </div>
-          <div>
-            {decks.length > 0 && (decks.map((deck) => (
-              <div className="card" style={{ width: "18rem" }}>
-                <div className="card-body">
-                  <h5 className="card-title">{deck.name}</h5>
-                  <p className="card-text">
-                    {deck.description}
-                  </p>
-                  <button type="button" className="btn btn-secondary">
-              View
-            </button>
-            <button type="button" className="btn btn-primary">
-              Study
-            </button>
-            <button type="button" className="btn btn-danger">
-              Delete
-            </button>
+            <div>
+              <Link to="/decks/new" className="btn btn-secondary btn-lg">
+                + Create Deck
+              </Link>
+            </div>
+            {decks.length > 0 &&
+              decks.map((deck) => (
+                <div className="card" style={{ width: "100%" }}>
+                  <div className="card-body">
+                    <h5 className="card-title">{deck.name}</h5>
+                    <p className="card-text">{deck.description}</p>
+                    <Link to={`/decks/${deck.id}`} className="btn btn-secondary">
+                      View
+                    </Link>
+                    <Link to={`/decks/${deck.id}/study`} className="btn btn-primary">
+                      Study
+                    </Link>
+                    <Link className="btn btn-danger">
+                      Delete
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            )))}
+              ))}
           </div>
         </Route>
+        
+        
       </Switch>
     </div>
   );
