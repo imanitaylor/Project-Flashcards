d2989ab7114aece0bd79707d81817a66807343fa
Imani Taylor <imanir.taylor@gmail.com>
2021-11-09 10:42:29 -0600

    delete button functionality

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Route, Switch, Link } from "react-router-dom";
-
-import { listDecks } from "../src/utils/api";
+import { listDecks, deleteDeck } from "../src/utils/api";
 
 /*
 Include the Layout screen
@@ -16,21 +15,63 @@ Clicking the “Delete” button shows a warning message before deleting the dec
 
 function Home() {
   const [decks, setDecks] = useState({});
+  const ac = new AbortController();
+
+  const loadDecks = () => {
+    listDecks(ac.signal).then(setDecks).catch(console.error);
+  };
 
   useEffect(() => {
-    const ac = new AbortController();
     setDecks([]);
-    const loadDecks = () => {
-      listDecks(ac.signal).then(setDecks).catch(console.error);
-    };
     loadDecks();
     return () => ac.abort();
   }, [setDecks]);
 
-  //   const handleClick = (event) => {
-  //     event.preventDefault();
-  //     console.log("Submitted");
-  //   };
+
+  const handleDelete = (deckId) => {
+    const result = window.confirm(
+      "Delete this deck? \n\nYou will not be able to recover it."
+    );
+    if (result === true) {
+      deleteDeck(deckId).then(loadDecks);
+    }
+  };
+
+  
+  const deckDisplay =
+    decks.length > 0 &&
+    decks.map((deck) => (
+      <div className="card p-2 m-3" style={{ width: "100%" }}>
+        <div className="card-body">
+          <div className="row">
+            <h5 className="col card-title">{deck.name}</h5>
+            <h6 className="text-secondary">{decks.length} cards</h6>
+          </div>
+          <p className="card-text">{deck.description}</p>
+          <div className="row">
+            <div className="col">
+              <Link to={`/decks/${deck.id}`} className="btn btn-secondary m-2">
+                View
+              </Link>
+              <Link
+                to={`/decks/${deck.id}/study`}
+                className="btn btn-primary m-2"
+              >
+                Study
+              </Link>
+            </div>
+            <div>
+              <button
+                className="btn btn-danger m-2"
+                onClick={() => handleDelete(deck.id)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    ));
 
   return (
     <div className="app-routes">
@@ -42,37 +83,7 @@ function Home() {
                 + Create Deck
               </Link>
             </div>
-            {decks.length > 0 &&
-              decks.map((deck) => (
-                <div className="card p-2 m-3" style={{ width: "100%" }}>
-                  <div className="card-body">
-                    <div className="row">
-                      <h5 className="col card-title">{deck.name}</h5>
-                      <h6 className="text-secondary">{decks.length} cards</h6>
-                    </div>
-                    <p className="card-text">{deck.description}</p>
-                    <div className="row">
-                      <div className="col">
-                        <Link
-                          to={`/decks/${deck.id}`}
-                          className="btn btn-secondary m-2"
-                        >
-                          View
-                        </Link>
-                        <Link
-                          to={`/decks/${deck.id}/study`}
-                          className="btn btn-primary m-2"
-                        >
-                          Study
-                        </Link>
-                      </div>
-                      <div>
-                        <Link className="btn btn-danger m-2">Delete</Link>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))}
+            <div>{deckDisplay}</div>
           </div>
         </Route>
       </Switch>
