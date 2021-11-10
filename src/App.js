79163e07e3a3431eef357109a166965c9956c784
Imani Taylor <imanir.taylor@gmail.com>
2021-11-10 09:21:43 -0600

    added editdeck, addcard, editcard routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Home from "./Home";
 import CreateDeck from "./CreateDeck";
 import Study from "./Study";
 import Deck from "./Deck";
+import EditDeck from "./EditDeck";
+import AddCard from "./AddCard";
+import EditCard from "./EditCard";
 
 /**
  * App is a wrapper for <Layout>, you should not need to change this file.
@@ -27,6 +30,15 @@ function App() {
         </Route>
         <Route exact path="/decks/:deckId">
             <Deck />
+        </Route>
+        <Route exact path="/decks/:deckId/edit">
+            <EditDeck />
+        </Route>
+        <Route exact path="/decks/:deckId/cards/new">
+            <AddCard />
+        </Route>
+        <Route exact path="/decks/:deckId/cards/:cardId/edit">
+            <EditCard />
         </Route>
         <Route>
           <NotFound />
