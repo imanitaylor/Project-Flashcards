541a7c1721e4ceb2835b8bb597da940d28d02b8d
Imani Taylor <imanir.taylor@gmail.com>
2021-11-10 19:57:44 -0600

    put routes into index instead of app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Header from "./Layout/Header";
-import NotFound from "./Layout/NotFound"
-import Home from "./Home";
-import CreateDeck from "./CreateDeck";
-import Study from "./Study";
-import Deck from "./Deck";
-import EditDeck from "./EditDeck";
-import AddCard from "./AddCard";
-import EditCard from "./EditCard";
+import Layout from "./Layout";
 
 /**
  * App is a wrapper for <Layout>, you should not need to change this file.
@@ -17,31 +9,9 @@ import EditCard from "./EditCard";
 function App() {
   return (
     <div className="app-routes">
-      <Header />
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/decks/new">
-            <CreateDeck />
-        </Route>
-        <Route exact path="/decks/:deckId/study">
-            <Study />
-        </Route>
-        <Route exact path="/decks/:deckId">
-            <Deck />
-        </Route>
-        <Route exact path="/decks/:deckId/edit">
-            <EditDeck />
-        </Route>
-        <Route exact path="/decks/:deckId/cards/new">
-            <AddCard />
-        </Route>
-        <Route exact path="/decks/:deckId/cards/:cardId/edit">
-            <EditCard />
-        </Route>
-        <Route>
-          <NotFound />
+        <Route path="/">
+          <Layout />
         </Route>
       </Switch>
     </div>
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,14 +1,48 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import Home from "../Home";
+import CreateDeck from "../CreateDeck";
+import Study from "../Study";
+import Deck from "../Deck";
+import EditDeck from "../EditDeck";
+import AddCard from "../AddCard";
+import EditCard from "../EditCard";
 
 function Layout() {
   return (
     <>
       <Header />
       <div className="container">
-        {/* TODO: Implement the screen starting here */}
-        <NotFound />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/decks/new">
+            <CreateDeck />
+        </Route>
+        <Route exact path="/decks/:deckId/study">
+            <Study />
+        </Route>
+        <Route exact path="/decks/:deckId">
+            <Deck />
+        </Route>
+        <Route exact path="/decks/:deckId/edit">
+            <EditDeck />
+        </Route>
+        <Route exact path="/decks/:deckId/cards/new">
+            <AddCard />
+        </Route>
+        <Route exact path="/decks/:deckId/cards/:cardId/edit">
+            <EditCard />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
       </div>
     </>
   );
